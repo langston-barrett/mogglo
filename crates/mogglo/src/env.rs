13895d39a6a98da9e08d02fe5f66c0da3204bfe1
Langston Barrett <langston.barrett@gmail.com>
2023-10-27 14:57:29 -0400

    Fix clippy lints

--- crates/mogglo/src/env.rs
+++ crates/mogglo/src/env.rs
@@ -11,14 +11,11 @@ pub struct Env<'tree>(pub(crate) HashMap<Metavar, HashSet<Node<'tree>>>);
 impl<'tree> Env<'tree> {
     pub fn extend(&mut self, env: Env<'tree>) {
         for (mvar, bindings) in env.0 {
-            self.0
-                .entry(mvar)
-                .or_insert_with(HashSet::new)
-                .extend(bindings);
+            self.0.entry(mvar).or_default().extend(bindings);
         }
     }
 
     pub fn insert(&mut self, mvar: Metavar, node: Node<'tree>) {
-        self.0.entry(mvar).or_insert_with(HashSet::new).insert(node);
+        self.0.entry(mvar).or_default().insert(node);
     }
 }
